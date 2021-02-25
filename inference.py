87df7f463517ce0339058b1bbfa8659b5775798a
Shan-Ping Mao <hectormao1025@gmail.com>
2021-02-26 00:59:47 +0800

    fix: inference.py

--- inference.py
+++ inference.py
@@ -9,26 +9,17 @@ import data_utils
 
 
 if __name__ == '__main__':
-    model = Model(data_utils.char_count, tiny=True)
-    model.load_state_dict(torch.load('./weights/tiny/epoch40.pth'))
+    model = Model(data_utils.char_count)
+    model.load_state_dict(torch.load('./weights/epoch3.pth'))
 
 
-    num_examples = 2
+    num_examples = 10
     with torch.no_grad():
-        trainset = Dataset('data/sample', device=device, num_examples=num_examples)
-        trainloader = data.DataLoader(trainset, batch_size=num_examples, shuffle=True)
-        for i, (images, labels) in enumerate(trainloader):
+        testset = Dataset('data/sample', device=device, num_examples=num_examples)
+        testloader = data.DataLoader(testset, batch_size=num_examples, shuffle=True)
+        for i, (images, labels) in enumerate(testloader):
             preds = model(images)
             results = torch.argmax(preds, dim=1)
             char_acc, name_acc = calc_acc(labels, results)
-
-            results = torch.squeeze(results)
-            labels = torch.squeeze(labels)
-
-            print(labels.shape, results.shape)
-            print([dict_index2char[index] for index in labels[0].tolist()])
-            print([dict_index2char[index] for index in results[0].tolist()])
-            print([dict_index2char[index] for index in labels[1].tolist()])
-            print([dict_index2char[index] for index in results[1].tolist()])
 
             print(f'char_acc={char_acc}, name_acc={name_acc}')
